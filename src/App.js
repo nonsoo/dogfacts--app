b0fa2b36f6b025c7054ccc547cc6d1389760536f
Nonso <nonsootoh@gmail.com>
2021-09-08 12:04:32 -0400

    app works

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import "./App.css";
+import { useState } from "react";
+
+import data from "./data.json";
 
 function App() {
+  const [num, setNum] = useState(0);
+  const factsObject = data.data;
+
+  const getFact = () => {
+    setNum(Math.floor(Math.random() * factsObject.length));
+  };
+
+  const Indivfact = factsObject[num].fact;
   return (
     <div className="App">
       <div className="container">
-        <p className="title">dogs are awesome here's a random fact...</p>
-        <p className="fact">I am going to be a random fact</p>
+        <p className="title">dogs are awesome, here's a random fact...</p>
+        <p className="fact">{Indivfact}</p>
 
-        <button className="btn">Show another</button>
+        <button className="btn" onClick={() => getFact()}>
+          Show Another
+        </button>
       </div>
     </div>
   );
